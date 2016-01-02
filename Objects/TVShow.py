cf5ff50b5d046737a211048dfbc9d79eee1f45d6
Anthony Bloomer <ant0@protonmail.ch>
2016-01-02 07:34:04 +0000

    Update TVShow.py

--- Objects/TVShow.py
+++ Objects/TVShow.py
@@ -7,56 +7,56 @@ class TVShow:
     def __init__(self, movie_data):
         self.tvdata = movie_data
 
-    def get_id(self):
+    def id(self):
         return self.tvdata['id']
 
-    def get_name(self):
+    def name(self):
         return self.tvdata['name']
 
-    def get_overview(self):
+    def overview(self):
         return self.tvdata['overview']
 
-    def get_first_air_date(self):
+    def first_air_date(self):
         return self.tvdata['first_air_date']
 
-    def get_last_air_date(self):
+    def last_air_date(self):
         return self.tvdata['last_air_date']
 
-    def get_number_of_episodes(self):
+    def number_of_episodes(self):
         return self.tvdata['number_of_episodes']
 
-    def get_number_of_seasons(self):
+    def number_of_seasons(self):
         return self.tvdata['number_of_seasons']
 
-    def get_poster(self):
+    def poster(self):
         return self.tvdata['poster_path']
 
-    def get_backdrop_path(self):
+    def backdrop_path(self):
         return self.tvdata['backdrop_path']
 
-    def get_vote_average(self):
+    def vote_average(self):
         return self.tvdata['vote_average']
 
-    def get_vote_count(self):
+    def vote_count(self):
         return self.tvdata['vote_count']
 
-    def get_popularity(self):
+    def popularity(self):
         return self.tvdata['popularity']
 
-    def get_release_date(self):
+    def release_date(self):
         return self.tvdata['release_date']
 
-    def get_original_language(self):
+    def original_language(self):
         return self.tvdata['original_language']
 
-    def get_original_name(self):
+    def original_name(self):
         return self.tvdata['original_name']
 
-    def get_status(self):
+    def status(self):
         return self.tvdata['status']
 
-    def get_type(self):
+    def type(self):
         return self.tvdata['type']
 
-    def get_json(self):
+    def json(self):
         return json.dumps(self.tvdata)
