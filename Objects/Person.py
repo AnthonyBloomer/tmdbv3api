17d7bb346a4c147dd2f776f4850e13980b657f90
Anthony Bloomer <ant0@protonmail.ch>
2016-01-02 07:33:07 +0000

    Update Person.py

--- Objects/Person.py
+++ Objects/Person.py
@@ -7,29 +7,29 @@ class Person:
     def __init__(self, persondata):
         self.persondata = persondata
 
-    def get_id(self):
+    def id(self):
         return self.persondata['id']
 
-    def get_name(self):
+    def name(self):
         return self.persondata['name']
 
-    def get_biography(self):
+    def biography(self):
         return self.persondata['biography']
 
-    def get_birthday(self):
+    def birthday(self):
         return self.persondata['birthday']
 
-    def get_deathday(self):
+    def deathday(self):
         return self.persondata['deathday']
 
-    def get_profilepath(self):
+    def profilepath(self):
         return self.persondata['profile_path']
 
-    def get_place_of_birth(self):
+    def place_of_birth(self):
         return self.persondata['place_of_birth']
 
-    def get_homepage(self):
+    def homepage(self):
         return self.persondata['homepage']
 
-    def get_json(self):
+    def json(self):
         return json.dumps(self.persondata)
