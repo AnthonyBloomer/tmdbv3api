6e948416903416fe04b0a1c87f1f92daa27b487e
Anthony Bloomer <ant0@protonmail.ch>
2016-01-02 07:32:47 +0000

    Update Movie.py

--- Objects/Movie.py
+++ Objects/Movie.py
@@ -7,44 +7,44 @@ class Movie:
     def __init__(self, movie_data):
         self.movie_data = movie_data
 
-    def get_id(self):
+    def id(self):
         return self.movie_data['id']
 
     def adult(self):
         return self.movie_data['adult']
 
-    def get_title(self):
+    def title(self):
         return self.movie_data['title']
 
-    def get_overview(self):
+    def overview(self):
         return self.movie_data['overview']
 
-    def get_poster(self):
+    def poster(self):
         return self.movie_data['poster_path']
 
-    def get_backdrop_path(self):
+    def backdrop_path(self):
         return self.movie_data['backdrop_path']
 
-    def get_vote_average(self):
+    def vote_average(self):
         return self.movie_data['vote_average']
 
-    def get_vote_count(self):
+    def vote_count(self):
         return self.movie_data['vote_count']
 
-    def get_popularity(self):
+    def popularity(self):
         return self.movie_data['popularity']
 
-    def get_release_date(self):
+    def release_date(self):
         return self.movie_data['release_date']
 
-    def get_original_language(self):
+    def original_language(self):
         return self.movie_data['original_language']
 
-    def get_original_title(self):
+    def original_title(self):
         return self.movie_data['original_title']
 
     def belongs_to_collection(self):
         return self.movie_data['belongs_to_collection']
 
-    def get_json(self):
+    def json(self):
         return json.dumps(self.movie_data)
