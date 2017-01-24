33544339fd20a45e784452ea8c2bbb49dd905dc2
Anthony Bloomer <ant0@protonmail.ch>
2017-01-24 22:52:26 +0000

    Update

--- tests/tests.py
+++ tests/tests.py
@@ -8,8 +8,68 @@ class TMDbTests(unittest.TestCase):
     def setUp(self):
         self.tmdb = TMDb('')
 
-    def test_search(self):
-        movie = self.tmdb.search("Pokémon")
-        for l in movie:
-            print l.title
+    def test_get_movie(self):
+        pass
 
+    def test_get_movie_reviews(self):
+        pass
+
+    def test_get_movie_lists(self):
+        pass
+
+    def test_get_movie_videos(self):
+        pass
+
+    def test_get_movie_recommendations(self):
+        pass
+
+    def test_discover_movies(self):
+        pass
+
+    def test_discover_tv_shows(self):
+        pass
+
+    def test_get_latest_movie(self):
+        pass
+
+    def test_now_playing(self):
+        pass
+
+    def test_top_rated(self):
+        pass
+
+    def test_upcoming(self):
+        pass
+
+    def test_popular(self):
+        pass
+
+    def test_search(self):
+        pass
+
+    def test_similar(self):
+        pass
+
+    def test_get_tv_show(self):
+        pass
+
+    def test_get_latest_tv_show(self):
+        pass
+
+    def test_search_tv(self):
+        pass
+
+    def test_similar_shows(self):
+        pass
+
+    def test_popular_shows(self):
+        pass
+
+    def test_top_rated_shows(self):
+        pass
+
+    def test_get_person(self):
+        pass
+
+    def test_search_person(self):
+        pass
