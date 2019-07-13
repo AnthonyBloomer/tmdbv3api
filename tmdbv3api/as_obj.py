b81ca6d8753b234ae58e8aaf0b2a9e273277e02c
Anthony Bloomer <ant0@protonmail.ch>
2019-07-13 17:23:31 +0100

    encode as utf-8

--- tmdbv3api/as_obj.py
+++ tmdbv3api/as_obj.py
@@ -5,9 +5,9 @@ class AsObj:
 
     def __repr__(self):
         if 'name' in self.entries:
-            return self.entries['name']
+            return self.entries['name'].encode('utf-8')
 
         elif 'title' in self.entries:
-            return self.entries['title']
+            return self.entries['title'].encode('utf-8')
 
         return "TMDb Obj"
