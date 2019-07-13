3a57d241845aa01a18bf1eb99a4d22b79198bce0
Anthony Bloomer <ant0@protonmail.ch>
2019-07-13 18:17:54 +0100

    test

--- tmdbv3api/as_obj.py
+++ tmdbv3api/as_obj.py
@@ -8,9 +8,9 @@ class AsObj:
 
     def __repr__(self):
         if 'name' in self.entries:
-            return self.entries['name'].encode('utf8')
+            return self.entries['name'].encode('utf8').decode()
 
         elif 'title' in self.entries:
-            return self.entries['title'].encode('utf8')
+            return self.entries['title'].encode('utf8').decode()
 
         return "TMDb Obj"
