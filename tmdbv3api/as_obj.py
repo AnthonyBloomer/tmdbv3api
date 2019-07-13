50c4d4fa63d1a356c904c7c571a8cf9e33e63684
Anthony Bloomer <ant0@protonmail.ch>
2019-07-13 17:37:56 +0100

    test build

--- tmdbv3api/as_obj.py
+++ tmdbv3api/as_obj.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+
 class AsObj:
     def __init__(self, **entries):
         self.entries = entries
@@ -5,9 +9,9 @@ class AsObj:
 
     def __repr__(self):
         if 'name' in self.entries:
-            return self.entries['name'].encode('utf-8')
+            return self.entries['name']
 
         elif 'title' in self.entries:
-            return self.entries['title'].encode('utf-8')
+            return self.entries['title']
 
         return "TMDb Obj"
