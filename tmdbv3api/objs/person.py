e3f4bea9b77aec5435749013179df275d0c608b7
backrndsource <backrndsource@gmail.com>
2019-12-06 19:41:06 +0100

    Add support append_to_response for Person().details()

--- tmdbv3api/objs/person.py
+++ tmdbv3api/objs/person.py
@@ -16,13 +16,13 @@ class Person(TMDb):
         'images': '/person/%s/images'
     }
 
-    def details(self, person_id):
+    def details(self, person_id, append_to_response='videos,images'):
         """
         Get the primary person details by id.
         :param person_id: int
         :return:
         """
-        return AsObj(**self._call(self._urls['details'] % str(person_id), ''))
+        return AsObj(**self._call(self._urls['details'] % str(person_id), 'append_to_response=' + append_to_response))
 
     def images(self, person_id):
         """
