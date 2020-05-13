4aebfa59a67cab14e9b4c06bd5fb0176b581a81c
Anthony Bloomer <ant0@protonmail.ch>
2020-05-13 15:34:30 +0100

    Fixes #43

--- tmdbv3api/as_obj.py
+++ tmdbv3api/as_obj.py
@@ -3,14 +3,15 @@
 
 class AsObj:
     def __init__(self, **entries):
-        self.entries = entries
+        self.__entries = entries
         self.__dict__.update(entries)
 
     def __repr__(self):
-        if 'name' in self.entries:
-            return self.entries['name']
 
-        elif 'title' in self.entries:
-            return self.entries['title']
+        if 'name' in self.__entries:
+            return self.__entries['name']
+
+        elif 'title' in self.__entries:
+            return self.__entries['title']
 
         return "TMDb Obj"
