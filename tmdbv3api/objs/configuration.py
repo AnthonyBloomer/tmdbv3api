8dbece55e3e8091f2ceef27a248a5c53730cccb9
backrndsource <backrndsource@gmail.com>
2020-12-29 02:44:38 +0100

    Correct the description of Configuration methods.

--- tmdbv3api/objs/configuration.py
+++ tmdbv3api/objs/configuration.py
@@ -20,30 +20,30 @@ class Configuration(TMDb):
 
     def countries(self):
         """
-        Get the system wide configuration info.
+        Get the list of countries (ISO 3166-1 tags) used throughout TMDb.
         """
         return self._call(self._urls["countries"], "")
 
     def jobs(self):
         """
-        Get the system wide configuration info.
+        Get a list of the jobs and departments we use on TMDb.
         """
         return self._call(self._urls["jobs"], "")
 
     def languages(self):
         """
-        Get the system wide configuration info.
+        Get the list of languages (ISO 639-1 tags) used throughout TMDb.
         """
         return self._call(self._urls["languages"], "")
 
     def primary_translations(self):
         """
-        Get the system wide configuration info.
+        Get a list of the officially supported translations on TMDb.
         """
         return self._call(self._urls["primary_translations"], "")
 
     def timezones(self):
         """
-        Get the system wide configuration info.
+        Get the list of timezones used throughout TMDb.
         """
         return self._call(self._urls["timezones"], "")
