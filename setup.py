c0e3cb49ec84a0f4ac8bdf831ff3d848810cd47a
Anthony Bloomer <anthony.bloomer@icloud.com>
2021-07-15 20:11:24 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ class PublishCommand(Command):
 
 
 setup(name='tmdbv3api',
-      version='1.7.5',
+      version='1.7.6',
       description='A lightweight Python library for The Movie Database (TMDb) API.',
       long_description=long_descr,
       url='https://github.com/AnthonyBloomer/tmdbv3api',
